a3e86d034555e61c699c59fd416e27be75a8d72f
rajamannar88 <rajamannar88@gmail.com>
2025-09-10 22:23:19 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
       {/*---copy-rights text---*/}
             <div>
                <hr />
-               <p className='py-5 text-sm text-center' >Copyright © 2024 Medivora - All Right Reserved.</p>
+               <p className='py-5 text-sm text-center' >Copyright © 2025 Medivora - All Right Reserved.</p>
 
             </div>
     </div>
